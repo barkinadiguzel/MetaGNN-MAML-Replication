86b686076131e33123340db54623557d3de2f287
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-02-11 01:08:12 +0300

    Create fo_maml.py

---
+++ src/meta_learning/fo_maml.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+from copy import deepcopy
+
+
+class FOMAML(nn.Module):
+    def __init__(self, model, inner_lr=1e-2, outer_lr=1e-3):
+        super().__init__()
+
+        self.model = model
+        self.inner_lr = inner_lr
+        self.outer_opt = torch.optim.Adam(model.parameters(), lr=outer_lr)
+
+    def meta_step(self, task_batch):
+
+        meta_loss = 0.0
+
+        for support, query in task_batch:
+
+            fast_model = deepcopy(self.model)
+
+            support_loss = fast_model(*support)
+            grads = torch.autograd.grad(
+                support_loss,
+                fast_model.parameters()
+            )
+
+            for p, g in zip(fast_model.parameters(), grads):
+                p.data -= self.inner_lr * g.detach()
+
+            query_loss = fast_model(*query)
+            meta_loss += query_loss.detach()
+
+        meta_loss /= len(task_batch)
+
+        self.outer_opt.zero_grad()
+        meta_loss.backward()
+        self.outer_opt.step()
+
+        return meta_loss.item()
